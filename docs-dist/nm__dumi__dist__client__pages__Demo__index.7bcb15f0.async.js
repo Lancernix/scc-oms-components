3d098052970d8a9b19301c477b6a8754d9f4d25c
lipengfeng <lipengfeng@sf-express.com>
2024-02-19 16:22:53 +0800

    chore(configs): 优化一些配置
    修改pre-commit钩子
    修改lint-staged配置

---
+++ docs-dist/nm__dumi__dist__client__pages__Demo__index.7bcb15f0.async.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkscc_oms_components=self.webpackChunkscc_oms_components||[]).push([[9],{50691:function(v,n,e){e.r(n),e.d(n,{default:function(){return m}});var o=e(86435),t=e(57689),a=function(){var c=(0,o.UO)(),r=c.id,u=(0,o.WF)(),d=u.demos,f=d[r]||{},s=f.component;return s&&(0,t.createElement)(s)},m=a}}]);
