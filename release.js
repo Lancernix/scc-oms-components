8a200e309df3c5f5e3605c5bea0f20c6596f722c
lipengfeng <lipengfeng@sf-express.com>
2024-02-23 17:10:38 +0800

    chore(configs): 发包脚本优化

--- release.js
+++ release.js
@@ -3,6 +3,7 @@ import chalk from 'chalk';
 import inquirer from 'inquirer';
 
 const MASTER_BRANCH = 'master';
+const CLEAN_TEXT = 'nothing to commit, working tree clean';
 
 /**
  * 获取用户选择的发版类型并进行确认
@@ -25,6 +26,7 @@ async function checkReleaseType(isBeta) {
 /**
  * 判断当前分支是否是master
  * @returns {boolean}
+ * @returns {boolean}
  */
 function isMasterBranch() {
   const curBranch = execSync('git symbolic-ref --short HEAD').toString();
@@ -35,10 +37,20 @@ function isMasterBranch() {
   return isMaster;
 }
 
+function checkClean() {
+  const stdout = execSync('git status').toString();
+  if (!stdout.endsWith(CLEAN_TEXT)) {
+    console.log(chalk.red.bold('❌ 工作区存在未提交的改动，请提交后再进行后续操作'));
+    process.exit(0);
+  }
+}
+
 /**
- * 判断当前分支是否超前master分支，超前才可以发版
+ * 分支比较，判断是否符合发版条件
+ * @param {boolean} isBeta
+ * @returns {void}
  */
-function checkCommitDiff() {
+function compareWithOriginMaster(isBeta) {
   console.log(chalk.blue('info - 分支commit检测：'));
   const stdout = execSync('git remote -v').toString();
   const strArr = stdout.match(/^(.+?)\s/);
@@ -46,21 +58,29 @@ function checkCommitDiff() {
     const remoteName = strArr[1];
     execSync(`git fetch ${remoteName}`);
     const behind = execSync(`git rev-list HEAD..${remoteName}/master`).toString();
-    if (!!behind) {
-      console.log(chalk.red.bold('当前分支落后于master分支，请合并后再发版'));
-      process.exit(1);
-    }
-    const beyond = execSync(`git rev-list ${remoteName}/master..HEAD`).toString();
-    if (!beyond) {
-      console.log(chalk.red.bold('当前分支commit记录与master分支相同，内容没有更新，不需要发版'));
-      process.exit(1);
+    if (isBeta) {
+      if (!!behind) {
+        console.log(chalk.red.bold('❌ 当前分支落后于master分支，请合并后再发版'));
+        process.exit(0);
+      }
+      const beyond = execSync(`git rev-list ${remoteName}/master..HEAD`).toString();
+      if (!beyond) {
+        console.log(chalk.red.bold('❌ 当前分支commit记录与master分支相同，代码没有更新，不需要发版'));
+        process.exit(0);
+      } else {
+        console.log(chalk.green.bold('✅ 当前分支超前于master分支，可以发版'));
+      }
     } else {
-      console.log(chalk.green.bold('当前分支超前于master分支，可以发版'));
+      if (!!behind) {
+        console.log(chalk.red.bold('❌ 当前master分支落后于远程master分支，请pull后再发版'));
+        process.exit(0);
+      } else {
+        console.log(chalk.red.bold('✅ 符合要求，可以发版'));
+      }
     }
   }
 }
 
-// 获取库的最新版本，获取当前即将发布的版本
 
 async function doRelease(isBeta) {
   const answers = await inquirer.prompt([
@@ -77,7 +97,9 @@ async function doRelease(isBeta) {
     }
   ]);
   const type = answers.type;
-  const command = `pnpm version ${isBeta ? 'pre' : ''}${type}`;
+  // 发包前需要切换到官方源
+  execSync('npm config set registry https://registry.npmjs.org');
+  const command = `npm version ${isBeta ? 'pre' : ''}${type}`;
   exec(command, (error, stdout, stderr) => {
     if (error) {
       console.error(`无法执行此命令: ${error}`);
@@ -93,12 +115,11 @@ async function doRelease(isBeta) {
 
 async function main() {
   try {
+    checkClean();
     const isBeta = !isMasterBranch();
     const isContinue = await checkReleaseType(isBeta);
     if (isContinue) {
-      if (isBeta) {
-        checkCommitDiff();
-      }
+      compareWithOriginMaster(isBeta);
       await doRelease(isBeta);
     }
   } catch (error) {
